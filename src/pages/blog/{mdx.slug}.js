f3912c4d1e16d164c1b77b52cc146e93f6caf035
Gabriel Mendes de Souza Santiago <gabrielmssantiago@gmail.com>
2021-10-21 17:25:15 -0300

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,4 +1,5 @@
 import { graphql } from 'gatsby';
+import { getImage, GatsbyImage } from 'gatsby-plugin-image';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import * as React from 'react';
 import Layout from '../../components/Layout';
@@ -7,17 +8,35 @@ const BlogPost = ({ data }) => {
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage
+        image={getImage(data.mdx.frontmatter.hero_image)}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />
+      <p>
+        Credit:{' '}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          data.mdx.frontmatter.hero_image_credit_text
+        </a>
+      </p>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
 };
 
 export const query = graphql`
-  query MyQuery($id: String) {
+  query ($id: String) {
     mdx(id: { eq: $id }) {
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_text
+        hero_image_credit_link
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
